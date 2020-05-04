d7a0cd4de7bec1dfd406062f60bcc8ae623b3790
Stephen Dios <stephendiosdeveloper@gmail.com>
2020-05-04 12:59:40 -0600

    Added working animations

--- Player.gd
+++ Player.gd
@@ -1,10 +1,12 @@
 extends Area2D
+signal hit
 
 export var speed = 400 # pixel/sec that the player moves
 var screen_size # size of game window
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
 
 func _process(delta):
@@ -26,3 +28,11 @@ func _process(delta):
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "right"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
